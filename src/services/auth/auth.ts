8377a9fbda3fdc1b9a472ac28777cfb15f7e545c
Richard <computerichy@live.co.uk>
2018-02-02 16:35:43 +0000

    added login loader and error handler

--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,23 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment';
+ 
+import { catchError, map, tap } from 'rxjs/operators';
+import { of }         from 'rxjs/observable/of';
+import {Observable} from 'rxjs/Observable';
+
+import { Dialogs } from '@ionic-native/dialogs';
+
 
 @Injectable()
 export class AuthService {
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, 
+                private dialogs: Dialogs) {
     }
 
     login(email:string, password:string ) {
         return this.http.post('https://sunshinemobile.co.uk:8097/api/login', {email, password})
-          .do(res => this.setSession);
+          .pipe(
+             tap(authResult => {  
+              this.setSession(authResult);
+              return authResult;
+            }),
+            catchError(this.handleError('login', []))
+        );
+
     }
 
     private setSession(authResult) {
+    
+      const token = authResult.authToken;
+
+       
+      
+     
       const expiresAt = moment().add(authResult.expiresIn, 'second');
 
       localStorage.setItem('authToken', authResult.authToken);
-      localStorage.setItem('authExpiresAt', expiresAt.valueOf());
+      localStorage.setItem('authExpiresAt', `${expiresAt.valueOf()}`);
 
     }
 
@@ -43,5 +64,18 @@ export class AuthService {
       const expiresAt = JSON.parse(expiration);
       return moment(expiresAt);
     }
+ 
+    
+    private handleError<T> (operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+
+        console.log(error);
+    
+        this.dialogs.alert(`${operation} failed: ${error.message}`);
+
+        return of(result as T);
+      };
+    }
+
 
 }
